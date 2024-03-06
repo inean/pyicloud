0a2643e2a63402b0d3d9591731e30882c1fc9d34
Carlos Martín <inean.es@gmail.com>
2024-03-06 21:24:49 +0000

    Refactor logging in pyicloud/base.py and add new log module

--- pyicloud/base.py
+++ pyicloud/base.py
@@ -7,7 +7,6 @@ import json
 import logging
 from http import cookiejar
 from pprint import pformat
-from urllib import request
 
 from requests import Session
 from requests.cookies import cookiejar_from_dict
@@ -20,6 +19,7 @@ from pyicloud.exceptions import (
     PyiCloudFailedLoginException,
     PyiCloudServiceNotActivatedException,
 )
+from pyicloud.log import LOGGER, PyiCloudPasswordFilter, get_logger, log_request
 from pyicloud.services import (
     AccountService,
     CalendarService,
@@ -30,56 +30,6 @@ from pyicloud.services import (
     RemindersService,
     UbiquityService,
 )
-
-LOGGER = logging.getLogger(__name__)
-
-
-class PyiCloudPasswordFilter(logging.Filter):
-    """Password log hider."""
-
-    _ACTIVE_FILTERS = {}
-
-    @classmethod
-    def register(cls, instance, logger=LOGGER):
-        """Register the object to the active filters."""
-        if logger not in cls._ACTIVE_FILTERS:
-            cls._ACTIVE_FILTERS[logger] = {}
-        assert instance not in cls._ACTIVE_FILTERS[logger]
-        cls._ACTIVE_FILTERS[logger][instance] = None
-
-    @classmethod
-    def on_password_changed(cls, value):
-        """Update the password for the active filters."""
-        for logger in cls._ACTIVE_FILTERS:
-            for instance, password_filter in cls._ACTIVE_FILTERS[logger].items():
-                if password_filter:
-                    logger.removeFilter(password_filter)
-                if value:
-                    password_filter = cls(value)
-                    logger.addFilter(password_filter)
-                    cls._ACTIVE_FILTERS[logger][instance] = password_filter
-
-    def __init__(self, password):
-        super().__init__(password)
-
-    def filter(self, record):
-        message = record.getMessage()
-        if self.name in message:
-            record.msg = message.replace(self.name, "*" * 8)
-            record.args = ()  # Assign an empty tuple instead of an empty list
-        return True
-
-
-def log_request(func):
-    def wrapper(self, method, url, **kwargs):
-        # Charge logging to the right service endpoint
-        callee = inspect.stack()[2]
-        module = inspect.getmodule(callee[0])
-        logger = logging.getLogger(module.__name__).getChild("http")
-        logger.debug("%s %s %s", method, url, kwargs.get("data", ""))
-        return func(self, method, url, logger, **kwargs)
-
-    return wrapper
 
 
 class PyiCloudSession(Session):
@@ -129,12 +79,8 @@ class PyiCloudSession(Session):
 
         # init elements
         self._owner = owner
-
         # set password filter
-        callee = inspect.stack()[2]
-        module = inspect.getmodule(callee[0])
-        logger = logging.getLogger(module.__name__).getChild("http")
-        PyiCloudPasswordFilter.register(self, logger)
+        PyiCloudPasswordFilter.register(self, logger=get_logger("http"))
 
     def load_cookies_from_file(self, cookiejar_file):
         """Load cookies from file."""
---
+++ pyicloud/log.py
@@ -0,0 +1,59 @@
+import inspect
+import logging
+from venv import logger
+
+LOGGER = logging.getLogger(__name__)
+
+
+def get_logger(name):
+    """Get a logger for the given name."""
+    callee = inspect.stack()[3]
+    module = inspect.getmodule(callee[0])
+    print(module.__name__)
+    return logging.getLogger(module.__name__).getChild(name)
+
+
+def log_request(func):
+    def wrapper(self, method, url, **kwargs):
+        # Charge logging to the right service endpoint
+        logger = get_logger("http")
+        logger.debug("%s %s %s", method, url, kwargs.get("data", ""))
+        return func(self, method, url, logger, **kwargs)
+
+    return wrapper
+
+
+class PyiCloudPasswordFilter(logging.Filter):
+    """Password log hider."""
+
+    _ACTIVE_FILTERS = {}
+
+    @classmethod
+    def register(cls, instance, logger=LOGGER):
+        """Register the object to the active filters."""
+        if logger not in cls._ACTIVE_FILTERS:
+            cls._ACTIVE_FILTERS[logger] = {}
+        assert instance not in cls._ACTIVE_FILTERS[logger]
+        cls._ACTIVE_FILTERS[logger][instance] = None
+
+    @classmethod
+    def on_password_changed(cls, value):
+        """Update the password for the active filters."""
+        for logger in cls._ACTIVE_FILTERS:
+            for instance, password_filter in cls._ACTIVE_FILTERS[logger].items():
+                if password_filter:
+                    logger.removeFilter(password_filter)
+                if value:
+                    password_filter = cls(value)
+                    logger.addFilter(password_filter)
+                    cls._ACTIVE_FILTERS[logger][instance] = password_filter
+
+    def __init__(self, password):
+        super().__init__(password)
+
+    def filter(self, record):
+        message = record.getMessage()
+        if self.name in message:
+            record.msg = message.replace(self.name, "*" * 8)
+            record.args = ()  # Assign an empty tuple instead of an empty list
+        return True
