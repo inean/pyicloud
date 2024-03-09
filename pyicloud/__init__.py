955aec31546ae2aaeb2d8564a560d73e29b8bc7f
Carlos Martín <inean.es@gmail.com>
2024-03-09 19:09:23 +0100

    Add VS Code settings, update .gitignore, update pyicloud package, and add launch.json

--- pyicloud/__init__.py
+++ pyicloud/__init__.py
@@ -2,6 +2,18 @@
 
 import logging
 
-from pyicloud.base import PyiCloud, PyiCloudServices
-
+# Set up logging for this module
 logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+from .base import PyiCloud, PyiCloudServices
+
+__all__ = ["PyiCloud", "PyiCloudServices"]
+
+# Try to import the version number from the _version module.
+# If it's not available (e.g., during development), default to "dev".
+try:
+    from ._version import version  # type: ignore
+except ImportError:
+    version = "0.1"
+
+__version__ = version
